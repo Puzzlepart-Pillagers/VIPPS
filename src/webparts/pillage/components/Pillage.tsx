552fe4bcf2fdca1b157e9bfec532a8e4ff25e434
Remi <remi.blom-ohlsen@puzzlepart.com>
2020-02-06 21:57:22 +0100

    Add css grid and buttons with avatar positioning

--- src/webparts/pillage/components/Pillage.tsx
+++ src/webparts/pillage/components/Pillage.tsx
@@ -14,41 +14,80 @@ export default class Pillage extends React.Component<IPillageProps, {}> {
     return (
       <div className={styles.pillage}>
         <div className={styles.container}>
-          <div className={styles.row}>
+          <div className={styles.parent}>
+            <div className={styles.div1}>
+                <img src="/src/webparts/pillage/images/avatar.png" />
+              
+            </div>
+
+            <div className={styles.div2}>
+              <h1>DISPLAYNAME</h1>
+              
+            </div>
+
+            <div className={styles.div3}>
+
+              <div className={styles.button}>
+                <PrimaryButton text="Start raid" allowDisabledFocus />
+              </div>
+              <div className={styles.button}>
+                <PrimaryButton text="Train units" allowDisabledFocus />
+              </div>
+              <div className={styles.button}>
+                <PrimaryButton text="Merch trip" allowDisabledFocus />
+              </div>
+              <div className={styles.button}>
+                <PrimaryButton text="Upgrades" allowDisabledFocus />
+              </div>
+              <div className={styles.button}>
+                <PrimaryButton text="Hire mercs" allowDisabledFocus />
+              </div>
+            </div>
+            <div className={styles.div4}><h1>DIV4</h1></div>
+            <div className={styles.div5}><h1>DIV5</h1></div>
+            <div className={styles.div6}><h1>DIV6</h1></div>
+          </div>
+
+
+
+
+          {/* <div className={styles.row}>
             <div className={styles.column}>
               <span className={styles.title}><h1>Welcome to Pillage</h1></span>
             </div>
-            <div className={styles.column}>
-              <div className={styles.button}>
-                <PrimaryButton text="Start raid" allowDisabledFocus />
-                <span><p>stuff</p></span>
+
+              <div className={styles.column}>
+                <div className={styles.button}>
+                  <PrimaryButton text="Start raid" allowDisabledFocus />
+                  <span><p>stuff</p></span>
+                </div>
               </div>
-            </div>
-            <div className={styles.column}>
-              <div className={styles.button}>
-                <PrimaryButton text="Train units" allowDisabledFocus />
-                <span>stuff</span>
+              <div className={styles.column}>
+                <div className={styles.button}>
+                  <PrimaryButton text="Train units" allowDisabledFocus />
+                  <span>stuff</span>
+                </div>
               </div>
-            </div>
-            <div className={styles.column}>
-              <div className={styles.button}>
-                <PrimaryButton text="Merch trip" allowDisabledFocus />
-                <span>stuff</span>
+              <div className={styles.column}>
+                <div className={styles.button}>
+                  <PrimaryButton text="Merch trip" allowDisabledFocus />
+                  <span>stuff</span>
+                </div>
               </div>
-            </div>
-            <div className={styles.column}>
-              <div className={styles.button}>
-                <PrimaryButton text="Upgrades" allowDisabledFocus />
-                <span>stuff</span>
+              <div className={styles.column}>
+                <div className={styles.button}>
+                  <PrimaryButton text="Upgrades" allowDisabledFocus />
+                  <span>stuff</span>
+                </div>
               </div>
-            </div>
-            <div className={styles.column}>
-              <div className={styles.button}>
-                <PrimaryButton text="Hire mercs" allowDisabledFocus />
-                <span>stuff</span>
+              <div className={styles.column}>
+                <div className={styles.button}>
+                  <PrimaryButton text="Hire mercs" allowDisabledFocus />
+                  <span>stuff</span>
+                </div>
               </div>
-            </div>
-          </div>
+            
+          </div> */}
         </div>
       </div>
     );
