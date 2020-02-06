a5fa28c3c741195f788d4c4261073fa3eefd02e7
Remi <remi.blom-ohlsen@puzzlepart.com>
2020-02-06 23:50:14 +0100

    adjustment to header flexbox

--- src/webparts/pillage/components/Pillage.tsx
+++ src/webparts/pillage/components/Pillage.tsx
@@ -9,19 +9,31 @@ export default class Pillage extends React.Component<IPillageProps, {}> {
         <div className={styles.container}>
           <div className={styles.parent}>
             <div className={styles.header}>
+              <div className={styles.headerLeft}>
+                <div className={styles.image}>
+                  <img src="/src/webparts/pillage/images/avatar.png" />
+                </div>
+                <div className={styles.displayName}>
+                  <h1>DISPLAYNAME</h1>
+                </div>
+                <div className={styles.level}>
+                  <h1>99</h1>
+                </div>
+              </div>
 
-              <div className={styles.image}>
-                <img src="/src/webparts/pillage/images/avatar.png" />
+              <div className={styles.headerRight}>
+                <div className={styles.exp}>
+                  <h3>1337 EXP</h3>
+                </div>
+                <div className={styles.units}>
+                  <h3>420 UNITS</h3>
+                </div>
+                <div className={styles.currency}>
+                  <h3>69 PENNING</h3>
+                </div>
               </div>
-              <div className={styles.displayName}>
-                <h1>DISPLAYNAME</h1>
-              </div>
-              
-                
             </div>
-
             <div className={styles.nav}>
-
               <button className={styles.button}>Start raid</button>
               <button className={styles.button}>Train units</button>
               <button className={styles.button}>Merch trip</button>
