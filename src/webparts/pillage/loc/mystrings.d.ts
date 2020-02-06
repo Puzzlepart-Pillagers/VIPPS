1ec047451f021aa2131a74ceba46001323568b53
oisteinunnerud <oistein.unnerud@puzzlepart.com>
2020-02-06 18:34:27 +0100

    initial commit

---
+++ src/webparts/pillage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPillageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PillageWebPartStrings' {
+  const strings: IPillageWebPartStrings;
+  export = strings;
+}
