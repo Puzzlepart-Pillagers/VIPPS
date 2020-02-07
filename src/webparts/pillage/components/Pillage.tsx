5c9105825b2b3cc7d01bd4bf4af5aa0dcf67c186
Håvard Berg <havard.berg@puzzlepart.com>
2020-02-07 10:28:29 +0100

    Fetch

--- src/webparts/pillage/components/Pillage.tsx
+++ src/webparts/pillage/components/Pillage.tsx
@@ -5,56 +5,62 @@ import * as Helpers from '../helpers/helpers';
 import { IKing } from '../models/IKing';
 import { IUnit } from '../models/IUnit';
 
-const { useEffect, useState } = React;
-
-const fetchKing = async (userEmail: string) => {
-    const res = await fetch(`https://pillagers-storage-functions.azurewebsites.net/api/GetKing?email=${userEmail}`);
-    const json = await res.json();
-    return Helpers.mapJsonToKing(json);
-  };
-
-  const fetchUnits = async (userEmail: string) => {
-    const res = await fetch(`https://pillagers-storage-functions.azurewebsites.net/api/GetUnits?email=${userEmail}`);
-    const json = await res.json();
-    return Helpers.mapJsonToUnits(json);
-  };
 
 
 
-export default class Pillage extends React.Component<IPillageProps, {}> {
+
+
+export default class Pillage extends React.Component<IPillageProps, { units?: IUnit[], isLoading: boolean; king: IKing }> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      king: null
+    }
+  }
+
+  public async componentDidMount() {
+    await this.fetchData();
+
+  }
 
   public render(): React.ReactElement<IPillageProps> {
     let userEmail = this.props.useremail;
-    let king = fetchKing(userEmail);
-    console.log(king);;
+
     return (
       <div className={styles.pillage}>
         <div className={styles.container}>
           <div className={styles.parent}>
-            <div className={styles.header}>
-              <div className={styles.headerLeft}>
-                <div className={styles.image}>
-                  <img src="/src/webparts/pillage/images/avatar.png" />
-                </div>
-                <div className={styles.displayName}>
-                  <h1>Odin</h1>
-                </div>
-                <div className={styles.level}>
-                  <p>99</p>
-                </div>
-              </div>
+            <div className={styles.header}>              
+              {!this.state.isLoading &&
+                <>
+                  <div className={styles.headerLeft}>
+                    <div className={styles.image}>
+                      <img src="/src/webparts/pillage/images/avatar.png" />
+                    </div>
+                    <div className={styles.displayName}>
+                      <h1>{this.state.king.firstName}</h1>
+                    </div>
+                    <div className={styles.level}>
+                      <p>99</p>
+                    </div>
+                  </div>
 
-              <div className={styles.headerRight}>
-                <div className={styles.exp}>
-                  <h3>1337 EXP</h3>
-                </div>
-                <div className={styles.units}>
-                  <h3>420 UNITS</h3>
-                </div>
-                <div className={styles.currency}>
-                  <h3>69 PENNING</h3>
-                </div>
-              </div>
+                  <div className={styles.headerRight}>
+                    <div className={styles.exp}>
+                      <h3>1337 EXP</h3>
+                    </div>
+                    <div className={styles.units}>
+                      <h3>{this.state.units.length} UNITS</h3>
+                    </div>
+                    <div className={styles.currency}>
+                      <h3>{this.state.king.penning} PENNING</h3>
+                    </div>
+                  </div>
+                </>
+              }
             </div>
             <div className={styles.nav}>
               <button className={styles.button}>Start raid</button>
@@ -69,4 +75,28 @@ export default class Pillage extends React.Component<IPillageProps, {}> {
       </div>
     );
   }
+  private async fetchKing(userEmail: string) {
+    const res = await fetch(`https://pillagers-storage-functions.azurewebsites.net/api/GetKing?email=${userEmail}`);
+    const json = await res.json();
+    return Helpers.mapJsonToKing(json);
+  };
+
+  private async fetchUnits(userEmail: string) {
+    const res = await fetch(`https://pillagers-storage-functions.azurewebsites.net/api/GetUnits?email=${userEmail}`);
+    const json = await res.json();
+    return Helpers.mapJsonToUnits(json);
+  };
+
+
+  private async fetchData() {
+
+    const king = await this.fetchKing(this.props.useremail);
+    if (king) {
+      const units = await this.fetchUnits(this.props.useremail)
+      this.setState({ king, units, isLoading: false });
+    }
+
+  }
+
+
 }
